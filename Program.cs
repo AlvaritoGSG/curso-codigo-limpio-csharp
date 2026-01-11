1aa204276dde6668ca8d8694f71fa6be8448f21a
AlvaritoGSG <AlvaritoGSG@outlook.com>
2026-01-11 00:00:40 -0400

    Buenas prácticas: DRY, KISS y Try-Catch: exceptions

--- Program.cs
+++ Program.cs
@@ -22,94 +22,114 @@ namespace ToDo
             do
             {
                 optionSelectedMenu = ShowMainMenu();
-                if ((option)optionSelectedMenu == option.Add)
+                if ((Option)optionSelectedMenu == Option.Add)
                 {
                     ShowAddOption();
                 }
-                else if (optionSelectedMenu == (int)option.Remove)
+                else if (optionSelectedMenu == (int)Option.Remove)
                 {
                     ShowRemoveOption();
                 }
-                else if ((option)optionSelectedMenu == option.ShowList)
+                else if ((Option)optionSelectedMenu == Option.ShowList)
                 {
                     ShowTasksOption();
                 }
-            } while (optionSelectedMenu != (int)option.Exit);
+            } while (optionSelectedMenu != (int)Option.Exit);
         }
-        const string separator = "==========================";
         public static int ShowMainMenu()
         {
-            Console.WriteLine("----------------------------------------");
-            Console.WriteLine("Ingrese la opción a realizar: ");
-            Console.WriteLine("1. Nueva tarea");
-            Console.WriteLine("2. Remover tarea");
-            Console.WriteLine("3. Tareas pendientes");
-            Console.WriteLine("4. Salir");
+            Console.WriteLine(" ______________________________________");
+            Console.WriteLine("|   Ingrese la opción a realizar:      |");
+            Console.WriteLine("|   1. Nueva tarea                     |");
+            Console.WriteLine("|   2. Remover tarea                   |");
+            Console.WriteLine("|   3. Tareas pendientes               |");
+            Console.WriteLine("|   4. Salir                           |");
+            Console.WriteLine("|______________________________________|");
             // Read line
             string line = Console.ReadLine();
             return Convert.ToInt32(line);
         }
         public static void ShowRemoveOption()
         {
+            if (Tasks == null || Tasks.Count == 0)
+            {
+                responder("No hay tareas por eliminar.");
+                return;
+            }
             try
             {
-                Console.WriteLine("Ingrese el número de la tarea a remover: ");
                 ShowTasksOption();
+                Console.WriteLine("[ c ] . Para cancelar operación de eliminar.");
+                // Personalizar respuesta para opción [ c ]. Aquí en try (No catch)
+                Console.WriteLine(" -> Ingrese el número de la tarea a remover: ");
                 string line = Console.ReadLine();
-                // Remove one position
                 int indexToRemove = Convert.ToInt32(line) - 1;
-                if (indexToRemove > -1 && Tasks.Count > 0)
+                if (indexToRemove < 0 || indexToRemove > Tasks.Count - 1)
+                {
+                    responder("Opción seleccionada fuera de rango.");
+                    ShowRemoveOption();
+                }
+                else if (indexToRemove > -1 && Tasks.Count > 0)
                 {
                     string task = Tasks[indexToRemove];
                     Tasks.RemoveAt(indexToRemove);
-                    Console.WriteLine(separator);
-                    Console.WriteLine("Tarea " + task + " eliminada");
-                    Console.WriteLine(separator);
+                    responder("Tarea: " + task + " ¡Eliminada!");
+                    ShowTasksOption();
                 }
             }
             catch (Exception)
             {
-                Console.WriteLine("Error al eliminar la tarea");
+                responder("Error al eliminar la tarea. Ingrese un opción válida.");
             }
         }
-
         public static void ShowAddOption()
         {
             try
             {
-                Console.WriteLine("Ingrese el nombre de la tarea: ");
+                Console.WriteLine("-> Ingrese el nombre de la tarea: ");
                 string task = Console.ReadLine();
-                Tasks.Add(task);
-                Console.WriteLine("Tarea registrada");
+                if (task != null && task.Trim() != "")
+                {
+                    Tasks.Add(task);
+                    responder($"Tarea: {task}, registrada con éxito!");
+                }
+                else
+                {
+                    responder("-> El nombre de la tarea no puede estar vacío, ¡Ingrese un dato válido!");
+                    ShowAddOption();
+                }
             }
             catch (Exception)
             {
-                Console.WriteLine("Error al agregar la tarea");
+                Console.WriteLine("Error al agregar la tarea.");
             }
         }
-
         public static void ShowTasksOption()
         {
             if (Tasks == null || Tasks.Count == 0)
             {
-                Console.WriteLine(separator);
-                Console.WriteLine("No hay tareas por realizar");
-                Console.WriteLine(separator);
+                responder("No hay tareas por pendientes/por realizar.");
             }
             else
             {
-                Console.WriteLine(separator);
+                Console.WriteLine($">>>>>>>>>>>>>>>>>>>>>>>>>>>>");
                 // foreach (var (t, i) in Tasks.Select((t, i) => (t, i + 1)))
                 foreach (var (t, i) in Tasks.ToIndexedEnumerable())
                 {
                     Console.WriteLine($"{i + 1}. {t}");
                 }
-                Console.WriteLine(separator);
+                Console.WriteLine($"<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
             }
+        }
+        public static void responder(string mensaje)
+        {
+            Console.WriteLine("================================");
+            Console.WriteLine(mensaje);
+            Console.WriteLine("================================");
         }
     }
 
-    public enum option
+    public enum Option
     {
         Add = 1,
         Remove = 2,
